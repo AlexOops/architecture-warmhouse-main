034e9fa9895259ba81cefc0425cf4d7743ef85b2
Alex <8768905213@mail.ru>
2025-12-09 21:13:37 +0300

    fix: temperature-api issue and update code

--- apps/temperature-api/main.go
+++ apps/temperature-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -13,49 +14,56 @@ type TemperatureResponse struct {
 	Value    float64 `json:"value"`
 }
 
+func temperatureHandler(w http.ResponseWriter, r *http.Request) {
+	location := r.URL.Query().Get("location")
+	sensorID := r.URL.Query().Get("sensorId")
+
+	if location == "" {
+		switch sensorID {
+		case "1":
+			location = "Living Room"
+		case "2":
+			location = "Bedroom"
+		case "3":
+			location = "Kitchen"
+		default:
+			location = "Unknown"
+		}
+	}
+
+	if sensorID == "" {
+		switch location {
+		case "Living Room":
+			sensorID = "1"
+		case "Bedroom":
+			sensorID = "2"
+		case "Kitchen":
+			sensorID = "3"
+		default:
+			sensorID = "0"
+		}
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	temp := 18 + rand.Float64()*10
+
+	resp := TemperatureResponse{
+		Location: location,
+		SensorID: sensorID,
+		Value:    temp,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
 func main() {
-	http.HandleFunc("/temperature", func(w http.ResponseWriter, r *http.Request) {
-		location := r.URL.Query().Get("location")
-		sensorID := r.URL.Query().Get("sensorId")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/temperature", temperatureHandler)
+	mux.HandleFunc("/temperature/", temperatureHandler)
 
-		if location == "" {
-			switch sensorID {
-			case "1":
-				location = "Living Room"
-			case "2":
-				location = "Bedroom"
-			case "3":
-				location = "Kitchen"
-			default:
-				location = "Unknown"
-			}
-		}
-
-		if sensorID == "" {
-			switch location {
-			case "Living Room":
-				sensorID = "1"
-			case "Bedroom":
-				sensorID = "2"
-			case "Kitchen":
-				sensorID = "3"
-			default:
-				sensorID = "0"
-			}
-		}
-
-		rand.Seed(time.Now().UnixNano())
-		temp := rand.Float64()*10 + 20 // 20–30 градусов
-
-		resp := TemperatureResponse{
-			Location: location,
-			SensorID: sensorID,
-			Value:    temp,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(resp)
-	})
-
-	http.ListenAndServe(":8081", nil)
+	log.Println("temperature-api is running on :8081")
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		log.Fatal("server error:", err)
+	}
 }
